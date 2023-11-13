a3798926a897636928ae86cd70a5f75b5e7e064a
Balamurugan Selvam <balan.job@gmail.com>
2023-11-13 14:07:36 +0530

    Added the button and Intro screen

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+l
+<app/src/main/java/com/example/poc_appbarcompose/BookModel.kt,9\2\92a97ac1b680505038abac2af10c35cf1bbd0e14
+u
+Eapp/src/main/java/com/example/poc_appbarcompose/AppBarWithScaffold.kt,4\8\48f62f1f5c1e52881cce53ad27de8032d3afa99e
+o
+?app/src/main/java/com/example/poc_appbarcompose/MainActivity.kt,f\6\f6e381f4bb9f048c70fe5d6190e142071b9981b2
+l
+<app/src/main/java/com/example/poc_appbarcompose/TopAppBar.kt,0\c\0c4d972383972c7a459c735543ab7896c9d5f210
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
---
+++ app/src/main/java/com/example/poc_appbarcompose/screens/intro/IntroBegin.kt
@@ -0,0 +1,48 @@
+package com.example.poc_appbarcompose.screens.intro
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.poc_appbarcompose.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun IntroBegin(navController: NavController) {
+    Scaffold(topBar = { TopAppBar(title = { /*TODO*/ }) }) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp)
+                .padding(it),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = stringResource(id = R.string.intro_begin),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.next))
+            }
+        }
+    }
+
+}
